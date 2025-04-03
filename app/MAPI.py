68dc50c6d1693284eac7180722f775bde3c36ac4
Kris Leungwattanakij <kleungwa@purdue.edu>
2025-04-02 20:46:22 -0400

    [feat] added and tested FetchStreams endpoint

--- app/MAPI.py
+++ app/MAPI.py
@@ -39,11 +39,13 @@ class AddStream:
             
             if (not streamIP or not streamName):
                 resp.status = falcon.HTTP_400
+                resp.media = {"error": "Cannot process request with empty stream ip."}
                 return
             
             alreadyExists = db.query(PublicStream).filter_by(streamIP=streamIP).first()
             if (alreadyExists):
                 resp.status = falcon.HTTP_400
+                resp.media = {"error": "A stream with this ip already exists."}
                 return
                 
             newStreamItem = PublicStream(streamName = streamName, streamIP = streamIP)
@@ -68,12 +70,14 @@ class DeleteStream:
             
             if (not streamIP):
                 resp.status = falcon.HTTP_400
+                resp.media = {"error": "Cannot process request with empty stream ip."}
                 return
             
             streamToDelete = db.query(PublicStream).filter_by(streamIP=streamIP).first()
             
             if (not streamToDelete):
-                resp.status = falcon.HTTP_400
+                resp.status = falcon.HTTP_404
+                resp.media = {"error": "No stream found with the given stream ip."}
                 return
             
             db.delete(streamToDelete)
@@ -84,3 +88,23 @@ class DeleteStream:
             db.close()
 deleteStream = DeleteStream()
 app.add_route('/deletestream', deleteStream)
+
+#Endpoint for fetching all existing streams from the public stream database
+class FetchStreams:
+    async def on_get(self, req, resp):
+        db = SessionLocal()
+        try:
+            streams = db.query(PublicStream).all()
+            
+            resp.media = [
+                {
+                    "streamName": item.streamName,
+                    "streamIP": item.streamIP
+                }
+                for item in streams
+            ]
+            resp.status = falcon.HTTP_200
+        finally:
+            db.close()
+fetchStreams = FetchStreams()
+app.add_route('/fetchstreams', fetchStreams)
